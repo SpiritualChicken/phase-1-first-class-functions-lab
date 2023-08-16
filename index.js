041b65f38a0f019c7c6884c4517c0499a59fd69f
Charlie <cnewlinds@gmail.com>
2023-08-16 14:25:02 +1000

    function added

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0,2) 
+}
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(2) 
+}
+
+let selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers,
+]
+
+
+
+function createFareMultiplier(x) {
+    return function (fare) {
+        return fare * x;
+    }
+}
+
+
+const fareDoubler = function(fare) {
+    return fare * 2;
+}
+
+const fareTripler= function(fare) {
+    return fare * 3;
+}
+
+
+
+function selectDifferentDrivers(driver, f) {
+    if (f == returnFirstTwoDrivers) {
+         return returnFirstTwoDrivers(driver);
+    } else if (f == returnLastTwoDrivers) {
+            return returnLastTwoDrivers(driver) 
+    }
+}
